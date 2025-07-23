4253e7e21a66b3b0af1388fcf87fecc81eae812a
Ngoc Giang <giang.ngocdao@gmail.com>
2025-07-23 10:45:16 +0700

    update menu form overlay

--- modules/Menu/Views/item/index.blade.php
+++ modules/Menu/Views/item/index.blade.php
@@ -1,8 +1,12 @@
-@if($items->count())
+@if ($items->count())
     @can('menu.item.index')
         <div class="dd" id="nestable">
             <ol class="dd-list">
-                @include('menu::item.item', ['items' => $items->filter(function($item, $key) { return $item->parent_id == 0; })])
+                @include('menu::item.item', [
+                    'items' => $items->filter(function ($item, $key) {
+                        return $item->parent_id == 0;
+                    }),
+                ])
             </ol>
         </div>
     @endcan
--- modules/Option/Routes/admin.php
+++ modules/Option/Routes/admin.php
@@ -3,23 +3,23 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group([
-  'prefix' => admin_path() . '/option',
+  'prefix' => admin_path(),
   'as'     => 'admin.option.',
   'middleware' => ['web', 'admin'],
   'namespace' => 'Modules\\Option\\Http\\Controllers'
 ], function () {
   // Option
-  Route::post('/', 'OptionController@save');
-  Route::get('/', 'OptionController@index');
+  Route::post('/option/', 'OptionController@save');
+  Route::get('/option/', 'OptionController@index');
 
   // general
-  Route::get('/general', 'OptionController@general');
-  Route::get('/system', 'OptionController@system');
-  Route::get('/update', 'OptionController@update');
-  Route::post('/update', 'OptionController@update');
+  Route::get('/option/general', 'OptionController@general');
+  Route::get('/option/system', 'OptionController@system');
+  Route::get('/option/update', 'OptionController@update');
+  Route::post('/option/update', 'OptionController@update');
 
   // email template
-  Route::get('/email', 'EmailController@email');
-  Route::get('/email/{module}/{email}', 'EmailController@editEmailTemplate');
-  Route::post('/email/{module}/{email}', 'EmailController@editEmailTemplate');
+  Route::get('/option/email', 'EmailController@email');
+  Route::get('/option/email/{module}/{email}', 'EmailController@editEmailTemplate');
+  Route::post('/option/email/{module}/{email}', 'EmailController@editEmailTemplate');
 });
